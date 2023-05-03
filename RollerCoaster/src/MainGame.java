3155b552a7fe603b283453d1162426bd94df9dcd
SussyBaka581 <clarketh@limestone.on.ca>
2023-05-03 14:08:08 -0400

    added commenting and begun coding of first 2 buttons

--- RollerCoaster/src/MainGame.java
+++ RollerCoaster/src/MainGame.java
@@ -4,15 +4,19 @@ import java.awt.event.*;
 import java.util.*;
 
 public class MainGame {
-
+        
+        //create frame method
         private JFrame frame;
         
         public MainGame(){
+                
+                //setting frame sizing parameters
                 frame = new JFrame("Whack an Ethan");
                 frame.setExtendedState(frame.MAXIMIZED_BOTH);
                 frame.setUndecorated(true);
                 frame.setVisible(true);
 
+                //creating and adding properties to close button
                 JButton exit = new JButton("CLOSE", null);
                 exit.addActionListener(event -> this.handleButton(event));
                 exit.setBorder(BorderFactory.createEmptyBorder());
@@ -20,9 +24,26 @@ public class MainGame {
                 exit.setBackground(Color.RED);
                 frame.add(exit);
 
+                /*adds picture as asset to game*/ImageIcon icon = new ImageIcon("RollerCoaster/src/GameAsset2.png");
+                
+                //code for buttons 1-6
+                JButton hole1 = new JButton();
+                hole1.setIcon(icon);
+                hole1.setBorder(BorderFactory.createEmptyBorder());
+                hole1.setBounds(300, 300, 200, 200);
+
+                JButton hole2 = new JButton();
+                hole2.setIcon(icon);
+                hole2.setBorder(BorderFactory.createEmptyBorder());
+                hole2.setBounds(500, 300, 200, 200);
+
+                frame.add(hole1);
+                frame.add(hole2);
+
+                frame.repaint();
         }
 
-        private void handleButton(ActionEvent event) {
+        private void handleButton(ActionEvent event) { //closes program when close button is pressed
                 System.exit(0);
         }
  }
