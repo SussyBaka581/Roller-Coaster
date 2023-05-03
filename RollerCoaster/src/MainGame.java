3c95562cf9fccc779a97c53e0291a8e91ba4972e
SussyBaka581 <clarketh@limestone.on.ca>
2023-05-03 14:31:04 -0400

    Finished the other 4 buttons and added colour

--- RollerCoaster/src/MainGame.java
+++ RollerCoaster/src/MainGame.java
@@ -14,6 +14,7 @@ public class MainGame {
                 frame = new JFrame("Whack an Ethan");
                 frame.setExtendedState(frame.MAXIMIZED_BOTH);
                 frame.setUndecorated(true);
+                frame.getContentPane().setBackground(Color.CYAN);
                 frame.setVisible(true);
 
                 //creating and adding properties to close button
@@ -30,15 +31,45 @@ public class MainGame {
                 JButton hole1 = new JButton();
                 hole1.setIcon(icon);
                 hole1.setBorder(BorderFactory.createEmptyBorder());
-                hole1.setBounds(300, 300, 200, 200);
+                hole1.setBounds(500, 300, 200, 200);
+                hole1.setBackground(Color.BLACK);
 
                 JButton hole2 = new JButton();
                 hole2.setIcon(icon);
                 hole2.setBorder(BorderFactory.createEmptyBorder());
-                hole2.setBounds(500, 300, 200, 200);
+                hole2.setBounds(750, 300, 200, 200);
+                hole2.setBackground(Color.BLACK);
+                
+                JButton hole3 = new JButton();
+                hole3.setIcon(icon);
+                hole3.setBorder(BorderFactory.createEmptyBorder());
+                hole3.setBounds(1000, 300, 200, 200);
+                hole3.setBackground(Color.BLACK);
+
+                JButton hole4 = new JButton();
+                hole4.setIcon(icon);
+                hole4.setBorder(BorderFactory.createEmptyBorder());
+                hole4.setBounds(500, 550, 200, 200);
+                hole4.setBackground(Color.BLACK);
+
+                JButton hole5 = new JButton();
+                hole5.setIcon(icon);
+                hole5.setBorder(BorderFactory.createEmptyBorder());
+                hole5.setBounds(750, 550, 200, 200);
+                hole5.setBackground(Color.BLACK);
+
+                JButton hole6 = new JButton();
+                hole6.setIcon(icon);
+                hole6.setBorder(BorderFactory.createEmptyBorder());
+                hole6.setBounds(1000, 550, 200, 200);
+                hole6.setBackground(Color.BLACK);
 
                 frame.add(hole1);
                 frame.add(hole2);
+                frame.add(hole3);
+                frame.add(hole4);
+                frame.add(hole5);
+                frame.add(hole6);
 
                 frame.repaint();
         }
