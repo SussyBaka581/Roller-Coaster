c3ba16b1b1c6198465acfb86ab7194d6fe70b187
SussyBaka581 <130686239+SussyBaka581@users.noreply.github.com>
2023-05-06 16:36:53 -0400

    Figured out hole 6 causes the error

--- RollerCoaster/src/MainGameTest.java
+++ RollerCoaster/src/MainGameTest.java
@@ -12,7 +12,13 @@ public class MainGameTest {
     private int hitCounter = 0;
     private int randNum = 0;
     private JButton hole1;
+    private JButton hole2;
+    private JButton hole3;
+    private JButton hole4;
+    private JButton hole5;
+    private JButton hole6;
     private JLabel me;
+    private ImageIcon icon;
 
     private void handleButton2(ActionEvent event) {
         ethanHit();
@@ -59,52 +65,52 @@ public class MainGameTest {
         exit.setBackground(Color.RED);
         frame.add(exit);
 
-        /* adds picture as asset to game */ImageIcon icon = new ImageIcon("RollerCoaster/src/GameAsset2.png");
+        /* adds picture as asset to game */icon = new ImageIcon("RollerCoaster/src/GameAsset2.png");
 
         // code for buttons 1-6
         hole1 = new JButton();
         hole1.addActionListener(event -> this.handleButton2(event));
-        //hole1.setIcon(icon);
+        // hole1.setIcon(icon);
         hole1.setBorder(BorderFactory.createEmptyBorder());
         hole1.setBounds(500, 300, 200, 200);
         hole1.setBackground(Color.BLACK);
 
-        JButton hole2 = new JButton();
+        hole2 = new JButton();
         hole2.addActionListener(event -> this.handleButton3(event));
         hole2.setIcon(icon);
         hole2.setBorder(BorderFactory.createEmptyBorder());
         hole2.setBounds(750, 300, 200, 200);
         hole2.setBackground(Color.BLACK);
 
-        JButton hole3 = new JButton();
+        hole3 = new JButton();
         hole3.addActionListener(event -> this.handleButton4(event));
         hole3.setIcon(icon);
         hole3.setBorder(BorderFactory.createEmptyBorder());
         hole3.setBounds(1000, 300, 200, 200);
         hole3.setBackground(Color.BLACK);
 
-        JButton hole4 = new JButton();
+        hole4 = new JButton();
         hole4.addActionListener(event -> this.handleButton5(event));
         hole4.setIcon(icon);
         hole4.setBorder(BorderFactory.createEmptyBorder());
         hole4.setBounds(500, 550, 200, 200);
         hole4.setBackground(Color.BLACK);
 
-        JButton hole5 = new JButton();
+        hole5 = new JButton();
         hole5.addActionListener(event -> this.handleButton6(event));
         hole5.setIcon(icon);
         hole5.setBorder(BorderFactory.createEmptyBorder());
         hole5.setBounds(750, 550, 200, 200);
         hole5.setBackground(Color.BLACK);
 
-        JButton hole6 = new JButton();
+        hole6 = new JButton();
         hole6.addActionListener(event -> this.handleButton7(event));
         hole6.setIcon(icon);
         hole6.setBorder(BorderFactory.createEmptyBorder());
         hole6.setBounds(1000, 550, 200, 200);
         hole6.setBackground(Color.BLACK);
 
-        JLabel me = new JLabel();
+        me = new JLabel();
         me.setIcon(icon);
 
         frame.add(hole1);
@@ -125,9 +131,21 @@ public class MainGameTest {
             randNum = (int) (Math.random() * 5) + 1;
             System.out.println(randNum);
             hitCounter++;
-            
+            holeAppear1();
+            holeAppear1();
+
         }
 
+    }
+
+    public void holeAppear1() {
+        if (randNum == 1) {
+            hole1.setIcon(icon);
+            hole1.setBorder(BorderFactory.createEmptyBorder());
+            hole1.setBounds(500, 300, 200, 200);
+            hole1.setBackground(Color.BLACK);
+            hole6.setVisible(false);
+        }
     }
 
 }
